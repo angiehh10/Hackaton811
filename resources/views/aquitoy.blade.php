bacfaada7a0015d36411cf505ff67ec28cbb06f1
angiehh10 <angiehhlobo10@gmail.com>
2025-08-20 11:22:11 -0600

    Update aquitoy.blade.php

--- resources/views/aquitoy.blade.php
+++ resources/views/aquitoy.blade.php
@@ -63,7 +63,7 @@
                     </ul>
                 </div>
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/323] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
-                    <img src="{{ asset('aquitoy.png') }}" alt="Aquitoy" class="w-full h-full object-contain" />
+                    <img src="{{ asset('hackaton.png') }}" alt="hackaton" class="w-full h-full object-contain" />
                     <div class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                 </div>
             </main>
